70a1fc1f7f5f6fc8bdea6f4f64927a7eafbe4310
ItsZmainDev <zmainpro@gmail.com>
2025-04-15 21:53:33 +0200

    Remove unused function

--- internal/checker/checker.go
+++ internal/checker/checker.go
@@ -1,26 +1,5 @@
 package checker
 
-import (
-    "bufio"
-    "os"
-)
-
 type Checker interface {
     Run() []string
-}
-
-func ReadFile(filename string) ([]string, error) {
-    file, err := os.Open(filename)
-    if err != nil {
-        return nil, err
-    }
-    defer file.Close()
-
-    var lines []string
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        lines = append(lines, scanner.Text())
-    }
-
-    return lines, scanner.Err()
 }
